7b5282fb4c5fc419ea344e469f076a61feb806c8
wstoop <warre.stoop@gmail.com>
2025-04-02 17:50:07 +0200

    Added bumping into player + cover big area

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,3 +1,6 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -18,17 +21,20 @@ public class BallController : MonoBehaviour
     private Vector2 _normalizedBallMovementInput = Vector2.zero;
     private float _holdTime = 0;
     private bool _isDashing = false;
-    private float _chargeRate = 1f;
     private float _maxDashForce = 10f;
-    private float _dashForce = 0;
     private AudioSource _audioSource;
     private bool _startGame = false;
+    private float _dashCoolDown = 3f;
+    private float _knockbackCooldown = 0f;
+
+    private List<GameObject> _tilesInRadius = new List<GameObject>();
 
     public void Awake()
     {
         _maxDashForce = 75 * _movementForce;
         _audioSource = GetComponent<AudioSource>();
     }
+
     public void OnMove(InputAction.CallbackContext context)
     {
         _normalizedBallMovementInput = context.ReadValue<Vector2>();
@@ -51,44 +57,87 @@ public class BallController : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if(_knockbackCooldown > 0)
+        {
+            _knockbackCooldown -= Time.deltaTime;
+        }
+        _dashCoolDown -= Time.deltaTime;
+    }
+
     public void OnDash(InputAction.CallbackContext context)
     {
-        if(!_startGame) return;
+        if (_dashCoolDown > 0 || !_startGame) return;
+
         if (context.started)
         {
-            Debug.Log("Dash started");
             _isDashing = true;
-            _holdTime = 0; // Reset hold time when dash starts
-        }
-        else if (context.performed)
-        {
-            Debug.Log("Dash performed");
-            _holdTime += Time.deltaTime;
-            _holdTime = Mathf.Clamp(_holdTime, 0, 1);
-            _dashForce = _maxDashForce * _holdTime;
-            Debug.Log(_dashForce);
         }
         else if (context.canceled)
         {
-            Debug.Log("Dash canceled");
+            if (!_isDashing) return;
+            float dashForce = _maxDashForce * 2 * (Mathf.Clamp(_holdTime, 0f, 1f) / 1f);
             Vector3 dashDirection = new Vector3(_normalizedBallMovementInput.x, 0f, _normalizedBallMovementInput.y);
             Vector3 torqueDirection = new Vector3(_normalizedBallMovementInput.y, 0f, -_normalizedBallMovementInput.x);
 
-            Debug.Log($"Applying Dash: {_dashForce}");
-            _rigidBody.AddForce(dashDirection * _dashForce, ForceMode.Impulse);
-            _rigidBody.AddTorque(100 * torqueDirection, ForceMode.Force);
-
+            _rigidBody.AddForce(dashDirection * dashForce, ForceMode.Impulse);
+            _rigidBody.AddTorque(100 * torqueDirection, ForceMode.Impulse);
             _audioSource.Play();
 
-            // Reset state
-            _holdTime = 0;
-            _dashForce = 0;
+            _knockbackCooldown = 0.5f;
+            _dashCoolDown = 3f;
             _isDashing = false;
+        }
+        else if (context.performed)
+        {
+            if (!_isDashing) return;
+            _holdTime += Time.deltaTime;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (_knockbackCooldown > 0 && collision.gameObject.CompareTag("Player"))
+        {
+            Rigidbody otherRigidBody = collision.gameObject.GetComponent<Rigidbody>();
+            if (otherRigidBody != null)
+            {
+                otherRigidBody.gameObject.GetComponent<BallController>().ExplosionEffect(this.gameObject.GetComponentInChildren<MeshRenderer>().material.color);
+                Vector3 launchDirection = (collision.transform.position - transform.position).normalized;
+                otherRigidBody.AddForce(launchDirection * (_maxDashForce / 50), ForceMode.Impulse);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Tile"))
+        {
+            Debug.Log("Hellnah");
         }
     }
 
     public void StartGame()
     {
         _startGame = true;
+    }
+
+    public void AddTileInRadius(GameObject tile)
+    {
+        _tilesInRadius.Add(tile);
+    }
+
+    public void RemoveTileInRadius(GameObject tile)
+    {
+        _tilesInRadius.Remove(tile);
+    }
+
+    public void ExplosionEffect(Color color)
+    {
+        foreach (GameObject tile in _tilesInRadius)
+        { 
+            tile.GetComponent<TileBehaviour>().SetColor(color);
+        }
     }
 }
